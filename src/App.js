386c1b3e9dc2a75507dc385a15fe6f55d20961c3
João Pedro Maciel <joomaciiel2@outlook.com>
2020-10-28 23:45:05 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // State (React Hooks)
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  // console.log(input);
+  // console.log(messages);
+
+  const sendMessage = (event) => {
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello, Messenger</h1>
+
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <button onClick={sendMessage}>Send Message</button>
+
+        { // MESSAGES
+          messages.map(message => (
+            <p>{message}</p>
+          ))
+        }
     </div>
   );
 }
